7cd242bd479e1bf25ea9278e47a49b95314f0589
anthony <anthonycolvil@gmail.com>
2019-11-23 00:01:46 -0400

    Implémentation des structure.

--- Bash-Link/main.c
+++ Bash-Link/main.c
@@ -1,15 +1,79 @@
+#include <stdio.h>
 //
 //  main.c
 //  Bash-Link
-//
 //  Created by Anthony Colvil on 21/11/2019.
 //  Copyright © 2019 Anthony Colvil. All rights reserved.
 //
 
-#include <stdio.h>
+
+//Structure de controle des utilisateurs
+
+
+//Structure des utilisateur
+typedef struct Utilisateur Utilisateur;
+struct Utilisateur {
+    Utilisateur * last;
+    char login[51];
+    int id ;
+    char motsPasse[51];
+    char nom[51];
+    char prenom[51];
+    char *id_amies;
+    int *id_postes ;
+    int *id_posts_likes ;
+    int *id_posts_commantes;
+    int âge;
+    Utilisateur * next;
+};
+
+typedef struct ListUtilisateur ListUser ;
+struct ListUtilisateur {
+    Utilisateur *tete ;
+    Utilisateur *queue ;
+    int nb_user ;
+};
+
+// poste d'utilisateur
+typedef struct post Post;
+struct post{
+    Post* last;
+    char *post;
+    int id_post;
+    int id_post_writer ;
+    Post *next;
+};
+
+typedef struct ListPost ListPost ;
+struct ListPost {
+    Post *tete ;
+    Post *queue ;
+    int nb_post ;
+};
+
+// commentaire d'utilisateur
+typedef struct comment Comment;
+struct comment{
+    Post* last;
+    char *comment;
+    int id_comment;
+    int id_post ;
+    Post *next;
+};
+
+typedef struct List ListComment ;
+struct ListComment {
+    Post *tete ;
+    Post *queue ;
+    int nb_Post ; ;
+};
+
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     return 0;
 }
+
